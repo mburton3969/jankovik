5f9547c7cf62135ce9a6a648793a54dee5b32717
Brooke Tweddell <brookealexandramedia.com>
2019-09-13 20:30:52 -0400

    update footer and index.php
    put in all info from previous site.

--- global/sections/footer.php
+++ global/sections/footer.php
@@ -3,56 +3,45 @@
         <div class="container">
           <div class="row row-30">
             <div class="col-sm-6 col-md-3">
-              <h1 class="heading-decorative">Links</h1>
+              <h1 class="heading-decorative">Home</h1>
               <ul class="list-marked text-primary font-weight-bold">
-                <li><a href="news.html">News & Events</a></li>
-                <li><a href="privacy-policy.html">Terms & Conditions</a></li>
-                <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                <li><a href="privacy-policy.html">Return Policy</a></li>
-                <li><a href="site-map.html">Site map</a></li>
-                <li><a href="overview.html">About Us</a></li>
-                <li><a href="news.html">Blog</a></li>
-                <li><a href="products.html">Franchise</a></li>
-                <li><a href="single-product.html">Ingredients</a></li>
+                <li><a href="about-us.php">About Us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="blog.php">Blog</a></li>
               </ul>
             </div>
             <div class="col-sm-6 col-md-4 col-lg-3">
-              <h1 class="heading-decorative">Categories</h1>
+              <h1 class="heading-decorative">Services</h1>
               <ul class="list-marked text-primary font-weight-bold">
-                <li><a href="products-for-face.html">Face</a></li>
-                <li><a href="products-for-face.html">Body</a></li>
-                <li><a href="products-for-face.html">Hair</a></li>
-                <li><a href="products-for-face.html">Hands-Feet</a></li>
-                <li><a href="products-for-face.html">Spa</a></li>
-                <li><a href="products-for-face.html">Baby</a></li>
-                <li><a href="products-for-face.html">For Men</a></li>
-                <li><a href="products-for-face.html">Sets</a></li>
+                <li><a href="chiropractic.php">Chiropractic</a></li>
+                <li><a href="massage.php">Massage</a></li>
+                <li><a href="allergy.php">Allergy</a></li>
+                <li><a href="nutrition.php">Nutrition</a></li>
               </ul>
             </div>
             <div class="col-sm-12 col-md-5 col-lg-4 col-xl-3">
               <h1 class="heading-decorative">Get in touch</h1>
               <ul class="contact-list">
-                <li>USA, LOS ANGELES,901 East E Street, Wilmington, CA 90744</li>
-                <li><a href="tel:#">+1 (800) 123 1234</a><br><span>E-mail:</span><a href="mailto:#"> mail@demolink.org</a></li>
+                <li>1309 Veale Road, Wilmington, DE 19810</li>
+                <li><a href="tel:#">1 (302) 384-8540</a><br><span>E-mail:</span><a href="mailto:#"> drstephenjankovic@gmail.com</a></li>
               </ul>
               <h1 class="heading-decorative">Newsletter</h1>
               <!-- RD Mailform-->
               <form class="rd-mailform rd-mailform-inline rd-mailform-icon text-left" data-form-output="form-output-global" data-form-type="forms" method="post" action="bat/rd-mailform.php">
                 <div class="form-wrap">
-                  <label class="form-label" for="inline-email-footer">info@demolink.org</label>
+                  <label class="form-label" for="inline-email-footer">mail@demolink.org</label>
                   <input class="form-input" id="inline-email-footer" type="email" name="email" data-constraints="@Email @Required">
                   <button class="button" type="submit"><span class="icon fa-envelope"></span></button>
                 </div>
               </form>
               <ul class="list-inline list-inline-sm">
                 <li><a class="icon fa-facebook" href="#"></a></li>
-                <li><a class="icon fa-google-plus" href="#"></a></li>
-                <li><a class="icon fa-linkedin" href="#"></a></li>
+                <li><a class="icon fa-instagram" href="#"></a></li>
                 <li><a class="icon fa-twitter" href="#"></a></li>
               </ul>
             </div>
           </div>
-          <p class="copyright"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>Natural Cosmetics</span><span>.&nbsp;</span><a href="privacy-policy.html">Privacy Policy</a>. Design&nbsp;by&nbsp;<a href="https://zemez.io/">Zemez</a></p>
+          <p class="copyright"><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>Ignition Innovations</span><span>.&nbsp;</span>
         </div>
       </footer>
     </div>
--- index.php
+++ index.php
@@ -40,24 +40,24 @@
               </div>
             </div>
             <div class="swiper-button swiper-button-next">
-              <div class="swiper-button__arrow">
+               <div class="swiper-button__arrow">  
                 <div class="icon icon-inverse mdi mdi-keyboard-backspace"></div>
               </div>
               <div class="preview">
                 <div class="preview__img"></div>
               </div>
             </div>
-          </div>
+          </div> 
           <div class="jumbotron-custom-wrapper">
             <div class="container">
               <div class="row justify-content-center">
                 <div class="col-md-9 col-xl-8">
                   <div class="jumbotron-custom">
-                    <div class="light-box"><img src="images/image-icon-1-49x43.png" alt="" width="49" height="43"/>
-                      <h2 class="title">Chemical Free, Natural Handmade Skin Care Lines</h2>
-                      <div class="exeption">100% Natural and Active ingredients, without water, mineral oils, propylene glycol and other petrochemicals, phthalates, perfumes, dyes, triethanolamine, parabens etc.</div>
+                    <div class="light-box" style="opacity: 0.8;"><img src="images/image-icon-1-49x43.png" alt="" width="49" height="43"/>
+                      <h2 class="title">Welcome to Jankovic Chiropractic</h2>
+                      <!-- <div class="exeption">100% Natural and Active ingredients, without water, mineral oils, propylene glycol and other petrochemicals, phthalates, perfumes, dyes, triethanolamine, parabens etc.</div> 
                     </div>
-                    <div class="primary-box"><a class="button button-white" href="products.html">View products</a></div>
+                    <div class="primary-box"><a class="button button-white" href="products.html">View products</a></div> -->
                   </div>
                 </div>
               </div>
@@ -65,28 +65,7 @@
           </div>
         </div>
       </section>
-      <section class="section section-lg bg-default">
-        <div class="container">
-          <div class="row row-30 justify-content-center">
-            <div class="col-md-6 d-flex">
-              <div class="cta bg-image bg-cta-1">
-                <div class="cta-inner">
-                  <h3 class="title">Wonderful <a href="#">gifts</a> from nature</h3>
-                  <h6 class="exeption">Tomato & Punica Granatum</h6><a class="button button-primary" href="products-for-face.html">View products</a>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6 d-flex">
-              <div class="cta bg-image bg-cta-2">
-                <div class="cta-inner">
-                  <h3 class="title">Beauty salon at home</h3>
-                  <h6 class="exeption">Personal SPA program</h6><a class="button button-primary" href="products-for-face.html">View products</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      
       <!-- Company overview-->
       <section class="section section-lg bg-default">
         <div class="container text-center">
@@ -94,11 +73,12 @@
           <p class="subtitle">about our company</p>
           <div class="row row-20 justify-content-around number-counter">
             <div class="col-md-6 text-left">
-              <p class="big">Over the years, Natural Cosmetics have created handmade natural cosmetics, by the principles of Corneotherapy, for professional use and has received recognition from beauticians of beauty salons and SPA centers. In the compositions of our Skin Care products the modern knowledge about the structure and functions of the skin is combined with the technologies that have proven their effectiveness.</p>
+              <p class="big">At Jankovic Chiropractic, Dr. Steve Jankovic provides both gentle chiropractic treatments and holistic health services. During each patient visit, he combines several holistic health modalities including chiropractic manipulation, kinesiology, nutrition evaluation, and a stress/emotional evaluation to optimize healing.</p>
             </div>
             <div class="col-md-6 text-left">
-              <p class="big">Over the years, we have improved the quality of our natural cosmetics. Under the new strategy of the company, cosmetics earlier supplied to salons and entirely new products are now proposed for home use according to salons techniques.  Our skin care products are fundamentally different from the “mass” cosmetics, by combining true naturalness and efficiency.</p>
+              <p class="big">Jankovic Chiropractic can provide many avenues of healing through chiropractic care, nutritional supplementation, learning and practicing stress reduction techniques. We are able to restore your health and it is our desire to help you live your best life. Many different approaches to health care are conidered as we desire the best way for your body to heal fully.</p>
             </div>
+            <!--
             <div class="col-md-3 text-gray">
               <div class="blurb-center">
                 <h2 class="index-counter"></h2>
@@ -119,12 +99,12 @@
                 <h4 class="blurb-title">Realism</h4>
                 <p class="blurb-content">Our advertisements do not include unreasonable promises</p>
               </div>
-            </div>
+            </div>-->
           </div>
-          <div class="button-custom-wrapper"><a class="button button-primary" href="overview.html">Read more about us</a></div>
+          <div class="button-custom-wrapper"><a class="button button-primary" href="about-us.php">Read more about us</a></div> 
         </div>
       </section>
-      <!-- OUR 100% healthy soaps-->
+      <!-- Chiropractic Section -->
       <section class="section section-lg bg-default">
         <div class="container text-center">
           <div class="row justify-content-center row-0">
@@ -135,15 +115,15 @@
             <div class="col-md-8 col-lg-6 section-xs-1">
               <div class="row justify-content-center">
                 <div class="col-lg-8">
-                  <h1 class="heading-decorative"><span><img class="image-center" src="images/typography-image-2-58x60.png" alt="" width="58" height="60"/>OUR 100% healthy soaps</span></h1>
-                  <p>We produce our soaps using Cold Process, a slow manufacturing process without external heat in order to ensure the high purity of the natural ingredients used. It makes our soaps healthy and fully biodegradable as well as ecological, and especially economic thanks to their longer service life.</p><a class="button button-primary" href="products.html">View products</a>
+                  <h1 class="heading-decorative"><span><img class="image-center" src="images/typography-image-2-58x60.png" alt="" width="58" height="60"/>Chiropractic Services</span></h1>
+                  <p>Chiropractic helps your body heal naturally by using spinal adjustments to correct vertebral misalignments in the spine. Dr. Jankovic uses many different techniques to correct vertebral subluxations, most of which are gentile, low force techniques.</p><a class="button button-primary" href="chiropractic.php">Learn More</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <!-- Take care of yourself and your skin-->
+      <!-- Massage Section -->
       <section class="section section-lg bg-default">
         <div class="container text-center">
           <div class="row justify-content-center row-0 flex-lg-row-reverse">
@@ -154,8 +134,31 @@
             <div class="col-md-8 col-lg-6 section-xs-1">
               <div class="row justify-content-center">
                 <div class="col-lg-8">
-                  <h1 class="heading-decorative"><span><img class="image-center" src="images/typography-image-3-57x60.png" alt="" width="57" height="60"/>Take care of yourself and your skin</span></h1>
-                  <p>Rediscover the pleasure of taking care of your skin. Enjoy our 100% natural superfatted soaps either upon awakening, for their invigorating and rejuvenating properties; or evening, to ease physical and mental tension accumulated during the day. Natural Cosmetics offers a range of  enriched soaps with essential oils and plant oils.</p><a class="button button-primary" href="products.html">View products</a>
+                  <h1 class="heading-decorative"><span><img class="image-center" src="images/typography-image-3-57x60.png" alt="" width="57" height="60"/>Massage Services</span></h1>
+                  <p>This is a description example of different massage services offered through Jankovic Chiropractic.</p><a class="button button-primary" href="massage.php">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Nutrition & Allergy Feature-->
+      <section class="section section-lg bg-default">
+        <div class="container">
+          <div class="row row-30 justify-content-center">
+            <div class="col-md-6 d-flex">
+              <div class="cta bg-image bg-cta-1">
+                <div class="cta-inner">
+                  <h3 class="title">Advanced <a href="allergy.php">Allergy</a> Therapeutics</h3>
+                  <h6 class="exeption">A highly effective, alternative therapy in treating allergies and sensitivities.</h6><a class="button button-primary" href="allergy.php">Learn More</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 d-flex">
+              <div class="cta bg-image bg-cta-2">
+                <div class="cta-inner">
+                  <h3 class="title">Nutrition Assessment</h3>
+                  <h6 class="exeption">Based on blood work analysis, organ and endocrine system evaluation.</h6><a class="button button-primary" href="products-for-face.html">Learn More</a>
                 </div>
               </div>
             </div>
@@ -169,47 +172,48 @@
             <div class="col-md-10 col-xl-8">
               <div class="row justify-content-center">
                 <div class="col-md-10">
-                  <h2 class="heading-decorative"><span><img src="images/typography-image-5-83x72.png" alt="" width="83" height="72"/>Change your lifestyle</span></h2>
-                  <p class="big">For the hygiene of your body, we developed ecological and economic vegan soaps made from natural ingredients such as essential oils and plant oils.</p>
+                  <h2 class="heading-decorative"><span><img src="images/typography-image-5-83x72.png" alt="" width="83" height="72"/>Change Your Lifestyle</span></h2>
+                  <p class="big">Care Chiropractic Center will educate and support people on their journeys toward health and well-being by introducing them to natural, gentle, noninvasive treatments.</p>
                 </div>
               </div>
               <div class="highlited-box offset-top-1">
-                <h1>Purchasing our 100% natural soaps is a first step towards a healthier lifestyle</h1>
+                <h1>Providing both gentle chiropractic treatments and holistic health services!</h1>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <!-- Change your lifestyle-->
+      <!-- Social Media-->
       <section class="section section-lg bg-default">
         <div class="container text-center">
-          <h1 class="heading-decorative"><span><img src="images/typography-image-2-58x60.png" alt="" width="58" height="60"/>Featured products</span></h1>
+          <h1 class="heading-decorative"><span><img src="images/typography-image-2-58x60.png" alt="" width="58" height="60"/>Social Media</span></h1>
           <!-- Owl Carousel-->
           <div class="owl-carousel" data-items="1" data-sm-items="1" data-md-items="2" data-lg-items="3" data-xl-items="3" data-xxl-items="3" data-dots="true" data-nav="true" data-stage-padding="28" data-xxl-stage-padding="0" data-loop="true" data-margin="30" data-mouse-drag="false">
             <div class="price-box">
               <div class="figure"><a href="single-product.html"><img src="images/product-category-7-370x270.jpg" alt="" width="370" height="270"/></a></div>
               <div class="caption">
-                <div class="title"><a href="single-product.html">Handmade Soap</a></div>
-                <p class="exeption">A true beauty, each piece of this handmade soap will put a smile on your face with the refreshing tingle of peppermint and the aroma of lemongrass.</p>
-                <p class="price">$25.90</p><a class="button button-primary" href="#">Buy on amazon</a>
+                <div class="title"><a href="blog.php">Blog Post Example</a></div>
+                <p class="exeption">This is an example blog post caption</p>
+                <a class="button button-primary" href="#">Blog Page</a>
               </div>
             </div>
             <div class="price-box">
               <div class="figure"><a href="single-product.html"><img src="images/product-category-8-370x270.jpg" alt="" width="370" height="270"/></a></div>
               <div class="caption">
-                <div class="title"><a href="single-product.html">Organic Makeup Set</a></div>
-                <p class="exeption">This set for makeup and skin care is considered the most popular and affordable beauty product, which can be ordered on the Web.Try it out!</p>
-                <p class="price">$35.80</p><a class="button button-primary" href="#">Buy on amazon</a>
+                <div class="title"><a href="blog.php">Instagram Post Example</a></div>
+                <p class="exeption">This is an example social media post caption</p>
+                <a class="button button-primary" href="#">Instagram Profile</a>
               </div>
             </div>
             <div class="price-box">
               <div class="figure"><a href="single-product.html"><img src="images/product-category-9-370x270.jpg" alt="" width="370" height="270"/></a></div>
               <div class="caption">
-                <div class="title"><a href="single-product.html">Beeswax Lip Balm</a></div>
-                <p class="exeption">Flavored with a hint of a citrusy tangerine oil, this rich balm serves up a matte finish that protects lips against the elements and has a sweet summer flavor.</p>
-                <p class="price">$12.90</p><a class="button button-primary" href="#">Buy on amazon</a>
+                <div class="title"><a href="blog.php">Facebook Post Example</a></div>
+                <p class="exeption">This is an example social media post caption</p>
+                <a class="button button-primary" href="#">Facebook Page</a>
               </div>
             </div>
+            <!--
             <div class="price-box">
               <div class="figure"><a href="single-product.html"><img src="images/product-category-7-370x270.jpg" alt="" width="370" height="270"/></a></div>
               <div class="caption">
@@ -234,7 +238,7 @@
                 <p class="price">$12.90</p><a class="button button-primary" href="#">Buy on amazon</a>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
       </section>
 <!-- *** Page Content Ends Here *** -->
