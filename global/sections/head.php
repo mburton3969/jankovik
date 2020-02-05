1166b31a6fd45e599427e99be942deda844d56a7
Michael Burton <michael@burtonsolution.com>
2020-02-05 16:57:29 -0600

    set up SEO in head.php

--- global/sections/head.php
+++ global/sections/head.php
@@ -1,7 +1,12 @@
     <!-- Site Title-->
-    <title>Jankovic</title>
+    <title>Jankovic Chiropractic & Wellness</title>
     <meta name="format-detection" content="telephone=no">
+    <meta name= "description" content="At Jankovic Chiropractic & Wellness, Dr. Steve Jankovic provides both gentle chiropractic treatments and holistic health services. During each patient visit, he combines several holistic health modalities, optimizing overall health and well-being and ultimately improving quality of life.">
+    <meta name= "keywords" content= "chiropractic,adjust chiropractic,family chiropractic,chiropractic care,health and wellness,scoliosis,what is scoliosis,sports chiropractic,holistic,chronic pain,chiropractor wilmington de,best chiropractic wilmington">
+    <meta name="google-site-verification" content="cN73CaG4l7i21ibVH7aqsdd9bJHDEymEujUXIOo_rwc" />
+    
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="robots" content="noindex" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8">
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
@@ -14,3 +19,14 @@
     <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
     <script src="js/html5shiv.min.js"></script>
 		<![endif]-->
+		
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157815276-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-157815276-1');
+</script>
+
